965944097878f414383a7d501773ba980486ed77
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-03 16:15:28 +0100

    Changes for life time calculater

--- js-basics-an-variables/lifetime-calculator/index.js
+++ js-basics-an-variables/lifetime-calculator/index.js
@@ -19,4 +19,25 @@ Hint: You can combine text and variables using the `+` operator.
 
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
-console.log("Your current age is: " + currentAge);
+const daysPerYear = 365;
+const lifeSpanInYears = 80;
+const sleepingTimeInHrs = 8;
+let livedDays = currentAge * daysPerYear;
+let remainingDays = lifeSpanInYears * daysPerYear - livedDays;
+
+let sleepingTimeInHrsTotal = sleepingTimeInHrs * livedDays;
+let sleepingTimeInDays = sleepingTimeInHrsTotal / 24;
+
+console.log(
+  "Your current age is: " +
+    currentAge +
+    " Years. You have lived for " +
+    livedDays +
+    " Days. Your remaining Days in your life are " +
+    remainingDays +
+    " Days. Your total sleeping time was " +
+    sleepingTimeInHrsTotal +
+    " hours. That are " +
+    sleepingTimeInDays +
+    " Days spended sleeping",
+);
