b89abd9d3b922faac38d270d47814ad36dd0c706
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-05 16:41:29 +0100

    Changes for statement challange.

--- js-conditions-and-booleans/statements/js/index.js
+++ js-conditions-and-booleans/statements/js/index.js
@@ -5,22 +5,46 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (SUPER_SECRET_PASSWORD == receivedPassword) {
+  console.log("Access denied!");
+} else {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+}
+
 // Part 2: Even / Odd
-const number = 6;
+const number = 7;
+
+if (number % 2 == 0) {
+  console.log("its even");
+} else {
+  console.log("Its odd");
+}
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 1;
+
+if (numberOfHotdogs < 5) {
+  console.log("2 euros per hotdog");
+} else if (numberOfHotdogs >= 5 && numberOfHotdogs < 100) {
+  console.log("1.50 euro per hotdog");
+} else if (numberOfHotdogs >= 100 && numberOfHotdogs < 1000000) {
+  console.log("1 euro per hotdog");
+} else if (numberOfHotdogs >= 1000000) {
+  console.log("0.10 euro per hotdog");
+}
 
 // Part 4: Daytime
 const currentHour = 12;
 
-const statement = "";
+let date = new Date();
+let hour = date.getHours();
 
+const statement = hour < 17 ? "Still need to learn..." : "Partytime!!!!";
 console.log(statement);
 
 // Part 5: Greeting
 const userName = "Archibald";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (userName == "Jakob" ? "Coach " : userName) + "!";
 
 console.log(greeting);
