44c96a4ef58b94853c6cdadb430db169f355ade5
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-09 14:36:14 +0100

    changes for uppercase challange

--- js-inputs-and-strings/input-to-uppercase/js/index.js
+++ js-inputs-and-strings/input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+
+const formInput = document.querySelector("[data-js=first-input]");
+const button = document.querySelector("[data-js=button-uppercase]");
+
+button.addEventListener("click", () => {
+  let input = formInput.value;
+  console.log(input.toUpperCase());
+});
