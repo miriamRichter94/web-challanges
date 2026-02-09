8bcac3bd398e3de349673d82e1d14439ce72b672
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-09 15:46:58 +0100

    changes for box appearance challange

--- js-inputs-and-strings/change-box-appearance/js/index.js
+++ js-inputs-and-strings/change-box-appearance/js/index.js
@@ -1,1 +1,18 @@
 console.clear();
+
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("change", () => {
+  box.style.background = "hsl(" + colorInput.value + ", 100%, 50%)";
+});
+
+radiusInput.addEventListener("change", () => {
+  box.style.borderRadius = radiusInput.value * 2 + "px";
+});
+
+rotationInput.addEventListener("change", () => {
+  box.style.transform = "rotate(" + rotationInput.value + "deg)";
+});
