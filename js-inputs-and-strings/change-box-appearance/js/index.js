c13755c450a5fb40020d29ead8947540bfc5a365
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-09 16:58:03 +0100

    Improvement changes

--- js-inputs-and-strings/change-box-appearance/js/index.js
+++ js-inputs-and-strings/change-box-appearance/js/index.js
@@ -5,14 +5,17 @@ const radiusInput = document.querySelector('[data-js="input-radius"]');
 const rotationInput = document.querySelector('[data-js="input-rotation"]');
 const box = document.querySelector('[data-js="box"]');
 
-colorInput.addEventListener("change", () => {
+// input macht es flüssiger als on CHange
+colorInput.addEventListener("input", () => {
   box.style.background = "hsl(" + colorInput.value + ", 100%, 50%)";
 });
 
-radiusInput.addEventListener("change", () => {
-  box.style.borderRadius = radiusInput.value * 2 + "px";
+radiusInput.addEventListener("input", () => {
+  // Prozent angabe und dann kann man *2 weglassen
+  //box.style.borderRadius = radiusInput.value * 2 + "px";
+  box.style.borderRadius = radiusInput.value + "%";
 });
 
-rotationInput.addEventListener("change", () => {
+rotationInput.addEventListener("input", () => {
   box.style.transform = "rotate(" + rotationInput.value + "deg)";
 });
