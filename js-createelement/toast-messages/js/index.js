047fa326e509d2db132c20f270e05b7c76c05ff0
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-10 12:12:22 +0100

    changes for toast messages

--- js-createelement/toast-messages/js/index.js
+++ js-createelement/toast-messages/js/index.js
@@ -6,8 +6,11 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  toastContainer.innerHTML +=
+    '<li class="toast-container__message">I\'m a new toast message.</li>';
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
