7cf5275d0cc185360879979366e8154739233939
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-10 15:24:47 +0100

    Changed setting of type in social media post AND changes for product teaser

--- js-createelement/social-media-post/js/index.js
+++ js-createelement/social-media-post/js/index.js
@@ -10,3 +10,30 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const article = document.createElement("article");
+const paragraph = document.createElement("p");
+const footer = document.createElement("footer");
+const span = document.createElement("span");
+const button = document.createElement("button");
+
+document.body.append(article);
+article.classList.add("post");
+paragraph.classList.add("post__content");
+paragraph.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+article.append(paragraph);
+footer.classList.add("post__footer");
+article.append(footer);
+span.classList.add("post__username");
+span.textContent = "@RavenVale";
+footer.append(span);
+button.classList.add("post__button");
+button.textContent = "♥ Like";
+footer.append(button);
+button.type = "button";
+//button.setAttribute("type", "button");
+button.setAttribute("data-js", "like-button2");
+
+const likeButton2 = document.querySelector('[data-js="like-button2"]');
+likeButton2.addEventListener("click", handleLikeButtonClick);
