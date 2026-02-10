e3bc7e2a140599f8132077779eec56297da4e796
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-10 11:24:43 +0100

    Changes for Form Submit

--- js-forms/handle-form-submit/js/index.js
+++ js-forms/handle-form-submit/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const badnessSumm = Number(data.age) + Number(data.badness);
+
+  console.log(
+    "The age-badness-sum of " + data.firstName + " is " + badnessSumm,
+  );
+
+  event.target.reset();
+  event.target.element.firstName.focus();
+});
