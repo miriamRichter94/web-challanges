efd7f33b8b72358613bf58d3f1b1aac02f800a10
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-10 11:43:07 +0100

    changes for checkbox input

--- js-forms/checkbox-input/js/index.js
+++ js-forms/checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successDiv = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,21 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data.hasOwnProperty("tos"));
+
+  if (!data.hasOwnProperty("tos")) {
+    showTosError();
+  } else {
+    hideTosError();
+    successDiv.removeAttribute("hidden");
+    event.target.reset();
+  }
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  //alert("Form submitted");
 });
