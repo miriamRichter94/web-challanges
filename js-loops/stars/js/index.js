4b0463e68259f935c36073f255a96249edfc326f
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-12 16:00:41 +0100

    Stars challenge changes

--- js-loops/stars/js/index.js
+++ js-loops/stars/js/index.js
@@ -2,13 +2,23 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
+  for (let i = 1; i <= 5; i++) {
+    const image = document.createElement("img");
+
+    if (i <= filledStars) {
+      image.src = "assets/star-filled.svg";
+    } else {
+      image.src = "assets/star-empty.svg";
+    }
+    starContainer.append(image);
+  }
 
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(Math.random() * (5 - 0) + 0);
