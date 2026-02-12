ec8c78f89bef9030f2035b60c0d1d83297d6cbbf
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-12 15:34:03 +0100

    Changes for while random number challange

--- js-loops/while-random-number/index.js
+++ js-loops/while-random-number/index.js
@@ -5,8 +5,14 @@ let count = 0;
 
 // --v-- write or modify code below this line --v--
 
+while (number <= 0.9) {
+  console.log(number);
+  number = Math.random();
+  count++;
+}
+
 // --^-- write or modify code above this line --^--
 
 console.log(
-  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
+  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`,
 );
