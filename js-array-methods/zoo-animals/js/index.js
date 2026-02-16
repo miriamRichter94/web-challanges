46344f4b5ccf0a4934b5ff6c23e482d8b381bb1c
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-16 16:17:25 +0100

    Zoo anmilas changes

--- js-array-methods/zoo-animals/js/index.js
+++ js-array-methods/zoo-animals/js/index.js
@@ -5,7 +5,7 @@ import { zooAnimals } from "../utils/db.js";
 // The function should return 'true' if the animal name is included in the array or 'false' if not.
 
 function hasAnimal(animals, animalName) {
-  return null;
+  return animals.includes(animalName);
 }
 
 // Bonus:
