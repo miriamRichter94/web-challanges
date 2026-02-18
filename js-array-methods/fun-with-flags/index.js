daa10f0df5b86a46f27b3d9f57a5dce876acf30a
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-18 10:33:47 +0100

    Filter one matching country

--- js-array-methods/fun-with-flags/index.js
+++ js-array-methods/fun-with-flags/index.js
@@ -9,7 +9,9 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  const foundCountry = countries.find(
+    (country) => country.name == searchString,
+  );
 
   if (foundCountry) {
     const countryElement = Country(foundCountry);
