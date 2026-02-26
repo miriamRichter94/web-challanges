01e2dc63ffbeff67754d3450d41cbfd8273a349d
miriamRichter94 <dark.rose2094@gmail.com>
2026-02-26 11:36:37 +0100

    changes to smiley challange

--- react-probs/smiley/src/App.jsx
+++ react-probs/smiley/src/App.jsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "😊" : "😟"}</h1>;
 }
--- react-probs/sum/src/App.jsx
+++ react-probs/sum/src/App.jsx
@@ -1,15 +1,13 @@
 export default function App() {
   return (
     <div>
-      <Sum valueA="3" valueB="4" />
-      <Sum valueA="8" valueB="3" />
-      <Sum valueA="2" valueB="6" />
+      <Sum valueA={3} valueB={4} />
+      <Sum valueA={8} valueB={3} />
+      <Sum valueA={2} valueB={6} />
     </div>
   );
 }
 
 function Sum({ valueA, valueB }) {
-  return (
-    <h1>{`${valueA} + ${valueB} = ${Number(valueA) + Number(valueB)}`}</h1>
-  );
+  return <h1>{`${valueA} + ${valueB} = ${valueA + valueB}`}</h1>;
 }
