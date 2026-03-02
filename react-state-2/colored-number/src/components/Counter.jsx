3ba277100481827bd2a0e93b31a87fc57675eef8
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-02 11:04:11 +0100

    Changes for colored number Challange

--- react-state-2/colored-number/src/components/Counter.jsx
+++ react-state-2/colored-number/src/components/Counter.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  const [value, setValue] = useState(0);
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={value} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={() => setValue(value + 1)}
         >
           +
         </button>
@@ -17,6 +20,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={() => setValue(value - 1)}
         >
           -
         </button>
