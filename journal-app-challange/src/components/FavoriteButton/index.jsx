b2cb7dba14b8d04c523083ec1f3e7d8eec59334b
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-02 16:19:26 +0100

    Changes for Favorite Button

--- journal-app-challange/src/components/FavoriteButton/index.jsx
+++ journal-app-challange/src/components/FavoriteButton/index.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import "./FavoriteButton.css";
 import StarFilled from "./star-filled.svg?react";
 import Star from "./star.svg?react";
 
 export default function FavoriteButton() {
   // This should be a state variable.
-  const isFavorite = false;
+  /* const isFavorite = false; */
+  const [isFavorite, setIsFavorite] = useState(false);
 
   return (
     <button
       className="favorite-button"
       onClick={() => {
+        setIsFavorite(!isFavorite);
         console.log("favorite button clicked");
       }}
       aria-label="favorite"
