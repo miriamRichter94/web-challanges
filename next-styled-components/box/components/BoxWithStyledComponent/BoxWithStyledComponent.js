455ed01cb65c980a873535264bfc72ae2502de42
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-13 16:37:10 +0100

    Changes for box challange

---
+++ next-styled-components/box/components/BoxWithStyledComponent/BoxWithStyledComponent.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ isBlack }) => {
+    return isBlack ? "black" : "green";
+  }};
+  margin: 2rem;
+  &:hover {
+    background-color: red;
+  }
+`;
--- next-styled-components/box/pages/index.js
+++ next-styled-components/box/pages/index.js
@@ -1,3 +1,4 @@
+import BoxWithStyledComponent from "@/components/BoxWithStyledComponent/BoxWithStyledComponent.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 
 export default function HomePage() {
@@ -5,6 +6,8 @@ export default function HomePage() {
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponent />
+      <BoxWithStyledComponent isBlack />
     </div>
   );
 }
