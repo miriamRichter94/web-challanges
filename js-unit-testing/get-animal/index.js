42d92393f3166767251c15e5aa3fc72cda833ea6
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-16 10:50:43 +0100

    Changes for animal Challange

--- js-unit-testing/get-animal/index.js
+++ js-unit-testing/get-animal/index.js
@@ -1,1 +1,12 @@
-export function getAnimal() {}
+export function getAnimal(animal = "") {
+  switch (animal) {
+    case "dogs":
+      return `I like ${animal}!`;
+    case "cats":
+      return `I totally love ${animal}!`;
+    case "":
+      return `I do not like animals at all!`;
+    default:
+      return "${animal} are okay";
+  }
+}
