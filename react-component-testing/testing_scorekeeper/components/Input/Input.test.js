48f94c57556643d4ce176b050e3b30bae2a0e149
miriamRichter94 <dark.rose2094@gmail.com>
2026-03-16 16:51:56 +0100

    Changes for testing/ not complete

--- react-component-testing/testing_scorekeeper/components/Input/Input.test.js
+++ react-component-testing/testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,27 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input labelText="Name" placeholder="Enter name" name="name" />);
+  const inputLabel = screen.getByText("Name");
+  expect(inputLabel).toBeInTheDocument();
 
-test("calls callback on every user input", async () => {});
+  const inputField = screen.getByRole("textbox", { name: "Name" });
+  expect(inputField).toHaveAttribute("placeholder", "Enter name");
+});
+
+test("calls callback on every user input", async () => {
+  const user = userEvent.setup();
+  const funct = jest.fn();
+  render(
+    <Input
+      labelText="Name"
+      placeholder="Enter name"
+      name="name"
+      onChange={funct}
+    />,
+  );
+  const inputField = screen.getByRole("textbox", { name: "Name" });
+  await user.type(inputField, "Mike");
+  expect(funct).toHaveBeenCalled();
+});
